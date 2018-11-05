9e5105fd2e36fa35913f6cd5af99044d3cca6f78
ErwannGui <erwann.guillevic@ynov.com>
2018-11-05 13:18:23 +0100

    add jwt

--- server/server.js
+++ server/server.js
@@ -2,20 +2,32 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
-    User = require('./api/models/crudModel'), //created model loading here
+    Model = require('./api/models/crudModel'), //created model loading here
     bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
+global.__root = __dirname + '/'; 
 mongoose.Promise = global.Promise;
-try {
-	//mongoose.connect('mongodb://localhost:27017/ionic');
-	mongoose.connect('mongodb+srv://ionic:ionic@cours-8uau7.mongodb.net/ionic');
-} catch(error) {
-  console.error(error);
-}
+//mongoose.connect('mongodb://localhost:27017/ionic');
+mongoose.connect('mongodb+srv://ionic:ionic@cours-8uau7.mongodb.net/ionic', { useNewUrlParser: true });
+
+/*var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb+srv://ionic:ionic@cours-8uau7.mongodb.net/ionic";
+MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+   //const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   //client.close();
+});*/
 
 var routes = require('./api/routes/crudRoutes'); //importing route
 routes(app); //register the route
+
+var UserController = require(__root + 'api/controllers/userController');
+app.use('/api/users', UserController);
+
+var AuthController = require(__root + 'api/controllers/authController');
+app.use('/api/auth', AuthController);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
